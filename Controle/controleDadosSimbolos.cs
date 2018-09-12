e531e2abd9d01b04bcffbfd2af6eaa00da4cd939
Lucas Santos Barbosa <39415128+lucassantos500@users.noreply.github.com>
2018-09-12 13:21:44 -0300

    Add files via upload

---
+++ Controle/controleDadosSimbolos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Controle
+{
+    public class controleDadosSimbolos
+    {
+        private string lexema, token, tipo;
+
+        public controleDadosSimbolos(string lexema, string token, string tipo)
+        {
+            this.lexema = lexema;
+            this.token = token;
+            this.tipo = tipo;
+        }
+
+        public string Lexema { get => lexema; set => lexema = value; }
+        public string Token { get => token; set => token = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+    }
+}
---
+++ Tabelas/tabelaSimbolo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+using Controle;
+
+namespace Tabelas
+{
+    class tabelaSimbolo
+    {
+        public static Hashtable tabelaS;
+
+        public static void startTabelaSimbolos()
+        {
+            tabelaSimbolo.tabelaS = new Hashtable();
+            controleDadosSimbolos simbolo1 = new controleDadosSimbolos("inicio", "inicio", " ");
+            tabelaSimbolo.tabelaS.Add("inicio", simbolo1);
+            controleDadosSimbolos simbolo2 = new controleDadosSimbolos("varinicio", "varinicio", " ");
+            tabelaSimbolo.tabelaS.Add("varinicio", simbolo2);
+            controleDadosSimbolos simbolo3 = new controleDadosSimbolos("varfim", "varfim", " ");
+            tabelaSimbolo.tabelaS.Add("varfim", simbolo3);
+            controleDadosSimbolos simbolo4 = new controleDadosSimbolos("escreva", "escreva", " ");
+            tabelaSimbolo.tabelaS.Add("escreva", simbolo4);
+            controleDadosSimbolos simbolo5 = new controleDadosSimbolos("leia", "leia", " ");
+            tabelaSimbolo.tabelaS.Add("leia", simbolo5);
+            controleDadosSimbolos simbolo6 = new controleDadosSimbolos("se", "se", " ");
+            tabelaSimbolo.tabelaS.Add("se", simbolo6);
+            controleDadosSimbolos simbolo7 = new controleDadosSimbolos("entao", "entao", " ");
+            tabelaSimbolo.tabelaS.Add("entao", simbolo7);
+            controleDadosSimbolos simbolo8 = new controleDadosSimbolos("fimse", "fimse", " ");
+            tabelaSimbolo.tabelaS.Add("fimse", simbolo8);
+            controleDadosSimbolos simbolo9 = new controleDadosSimbolos("fim", "fim", " ");
+            tabelaSimbolo.tabelaS.Add("fim", simbolo9);
+            controleDadosSimbolos simbolo10 = new controleDadosSimbolos("inteiro", "inteiro", " ");
+            tabelaSimbolo.tabelaS.Add("inteiro", simbolo10);
+            controleDadosSimbolos simbolo11 = new controleDadosSimbolos("literal", "literal", " ");
+            tabelaSimbolo.tabelaS.Add("literal", simbolo11);
+            controleDadosSimbolos simbolo12 = new controleDadosSimbolos("real", "real", " ");
+            tabelaSimbolo.tabelaS.Add("real", simbolo12);
+        }
+
+    }
+}
