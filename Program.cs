32bd92ca8946d53a6106c75f31c8399e94c48b75
Lucas Santos Barbosa <39415128+lucassantos500@users.noreply.github.com>
2018-10-01 21:29:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analisador;
+using Controle;
+using Tabelas;
+//Classe temporaria que representa a MAIN
+
+
+namespace CompiladorMgol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            controleDadosSimbolos simb;
+            do
+            {
+                simb = analisadorLexico.getLex(analisadorLexico.pos);
+                Console.WriteLine("--------------------");
+                Console.WriteLine("\nToken: " + simb.Token + " \nLexema: " + simb.Lexema + " \nTipo: " + simb.Tipo);
+                
+            } while (simb.Lexema != "EOF" && simb.Lexema != "ERRO");
+            Console.ReadKey();//Pausando para leitura
+        }
+    }
+}
