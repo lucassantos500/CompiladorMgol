7533b08053e77059f1d8e60716e837817f9d5f99
Lucas Santos Barbosa <39415128+lucassantos500@users.noreply.github.com>
2018-12-18 10:35:40 -0200

    Add files via upload

---
+++ Tabelas/tabelaSintatico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabelas
+{
+    public class tabelaSintatico
+    {
+        private int elemento;
+
+        public tabelaSintatico(int elemento)
+        {
+            this.elemento = elemento;
+        }
+
+        public int Elemento { get => elemento; set => elemento = value; }
+    }
+}
