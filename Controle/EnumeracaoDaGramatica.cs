64160152c1da0970470ff85b5106451fb2887876
Lucas Santos Barbosa <39415128+lucassantos500@users.noreply.github.com>
2018-12-18 10:35:18 -0200

    Add files via upload

---
+++ Controle/EnumeracaoDaGramatica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    public class EnumeracaoDaGramatica
+    {
+        private int enumeracao;
+        private String sentenca;
+
+        public EnumeracaoDaGramatica(int enumeracao, String sentenca)
+        {
+            this.Enumeracao = enumeracao;
+            this.Sentenca = sentenca;
+        }
+
+        public int Enumeracao { get => enumeracao; set => enumeracao = value; }
+        public string Sentenca { get => sentenca; set => sentenca = value; }
+    }
+}
